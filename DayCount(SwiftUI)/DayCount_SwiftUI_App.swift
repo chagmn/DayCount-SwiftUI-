51ab97e426b3af3006c10fba14735fd6cddbd0ba
chagmn <sopg4@naver.com>
2021-05-21 14:33:30 +0900

    Initial Commit

---
+++ DayCount(SwiftUI)/DayCount_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  DayCount_SwiftUI_App.swift
+//  DayCount(SwiftUI)
+//
+//  Created by 창민 on 2021/05/21.
+//
+
+import SwiftUI
+
+@main
+struct DayCount_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DayCount(SwiftUI)Tests/DayCount_SwiftUI_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  DayCount_SwiftUI_Tests.swift
+//  DayCount(SwiftUI)Tests
+//
+//  Created by 창민 on 2021/05/21.
+//
+
+import XCTest
+@testable import DayCount_SwiftUI_
+
+class DayCount_SwiftUI_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
