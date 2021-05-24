95949dceaf17a25b13662977e41cdc2760fd1ebd
chagmn <sopg4@naver.com>
2021-05-24 17:13:50 +0900

    update MainViewController

--- DayCount(SwiftUI)/ViewModel/MainViewController.swift
+++ DayCount(SwiftUI)/ViewModel/MainViewController.swift
@@ -8,6 +8,7 @@
 import Foundation
 import SwiftUI
 
+// 메인 뷰를 다루는 뷰 모델
 class MainViewController {
     // 해당 뷰모델에서 사용할 모델을 불러옴.
     private var model: DDay = DDay(title: "111", date: "111", isFromToday: false)
