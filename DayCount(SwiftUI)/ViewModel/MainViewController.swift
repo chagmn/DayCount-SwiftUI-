a9689f3cf805ceace06d43ab3a46fd2cda81859b
chagmn <sopg4@naver.com>
2021-05-24 16:45:00 +0900

    add mainViewModel

---
+++ DayCount(SwiftUI)/ViewModel/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewController.swift
+//  DayCount(SwiftUI)
+//
+//  Created by 창민 on 2021/05/24.
+//
+
+import Foundation
+import SwiftUI
+
+class MainViewController {
+    // 해당 뷰모델에서 사용할 모델을 불러옴.
+    private var model: DDay = DDay(title: "111", date: "111", isFromToday: false)
+}
