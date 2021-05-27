06acd69afad52951145da5f78227e0070f87b9dd
chagmn <sopg4@naver.com>
2021-05-27 18:16:44 +0900

    update AddItemView

--- DayCount(SwiftUI)/View/AddItemView.swift
+++ DayCount(SwiftUI)/View/AddItemView.swift
@@ -6,15 +6,25 @@
 //
 
 import SwiftUI
+import Combine
 
 // 디데이 항목 추가하는 뷰
 struct AddItemView: View {
     @State private var title: String = ""
     @State private var date: String = ""
-    @State private var isToggleOn: Bool = false
+    @State private var isToggleOn: Bool = true
     @State private var isClickedAddButton: Bool = false
+    @State private var dateValue: Date = Date()
     
-    @ObservableObject var addItemViewModel: AddItemViewModel = AddItemViewModel()
+    @Binding var showAddItemView: Bool
+    
+    @ObservedObject var addItemViewModel: AddItemViewModel = AddItemViewModel()
+    
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "YYYY/MM/dd"
+        return formatter
+    }
     
     var body: some View {
         VStack(alignment: .center){
@@ -26,38 +36,40 @@ struct AddItemView: View {
                 .cornerRadius(10)
                 .padding(.bottom, 30)
                            
-            HStack(spacing: 15){
-                TextField("연도 / 월 / 일", text: $date)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.black)
-                    .frame(width: 180, height: 40)
-                    .background(Color(#colorLiteral(red: 0.8564875722, green: 0.8513966799, blue: 0.8604011536, alpha: 1)))
-                    .cornerRadius(10)
-                    
-                
+            HStack(spacing: 50){
                 Toggle("오늘 기준", isOn: $isToggleOn)
                     .frame(width: 130, height: 40)
                 
-                if isToggleOn{
-                    
-                }
-                // 오늘 기준 토글이 on이 되면
-           
+                
+                DatePicker(selection: $dateValue, in: ...Date(), displayedComponents: .date){
+                   // Text("\(dateField, formatter: dateFormatter)")
+                }.labelsHidden()
+              
+                
             }.padding(.bottom, 50)
             
-            Button("추가하기"){
-                self.isClickedAddButton.toggle()
-            }.frame(width: 160, height: 35, alignment: .center)
+            Button("추가하기", action: {
+                addButtonDidTap()
+            }).frame(width: 160, height: 35, alignment: .center)
             .background(Color(UIColor.systemBlue))
             .foregroundColor(.white)
             .cornerRadius(10)
-        
+            
+            
         }
+    }
+    
+    func addButtonDidTap(){
+        self.addItemViewModel.title = title
+        self.addItemViewModel.date = dateFormatter.string(from: dateValue)
+        self.addItemViewModel.calcDDay()
+        self.showAddItemView = false
+        
     }
 }
 
 struct AddItemView_Previews: PreviewProvider {
     static var previews: some View {
-        AddItemView()
+        AddItemView(showAddItemView: .constant(true))
     }
 }
