c87b25770558b76b059530cfed240dff518c778a
chagmn <sopg4@naver.com>
2021-05-30 18:47:12 +0900

    update DDay Model

--- DayCount(SwiftUI)/Model/DDay.swift
+++ DayCount(SwiftUI)/Model/DDay.swift
@@ -19,4 +19,24 @@ struct DDay {
         self.date = date
         self.isFromToday = isFromToday
     }
+    
+    func calcDDay() -> String{
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy년 MM월 dd일"
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        dateFormatter.timeZone = TimeZone(abbreviation: "UTC") as TimeZone?
+        let todayDate: Date = dateFormatter.date(from: dateFormatter.string(from: Date()))!
+        let ddayDate: Date = dateFormatter.date(from: date)!
+        
+        var interval: TimeInterval
+        
+        if isFromToday {    // 오늘부터 'on'
+            interval = todayDate.timeIntervalSince(ddayDate)
+            return "D+"+String(Int(interval/86400))
+        } else {
+            interval = ddayDate.timeIntervalSince(todayDate)
+            return "D-"+String(Int(interval/86400))
+        }
+        
+    }
 }
