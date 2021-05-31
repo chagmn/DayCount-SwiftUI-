638af9817207304e2bb44239c0f158e46cda8341
chagmn <sopg4@naver.com>
2021-05-31 19:49:17 +0900

    update MainViewModel

--- DayCount(SwiftUI)/ViewModel/MainViewModel.swift
+++ DayCount(SwiftUI)/ViewModel/MainViewModel.swift
@@ -5,14 +5,14 @@
 //  Created by 창민 on 2021/05/25.
 //
 
-import Foundation
+import SwiftUI
 import Combine
 
-class MainViewModel: ObservableObject {
+class MainViewModel: ObservableObject, Identifiable {
+    // 모델이 바뀌면 뷰를 업데이트 해주라는 의미
     @Published var ddaylist: [DDay] = []
     
-    
     func addDDay(title: String, date: String, isFromToday: Bool){
-        ddaylist.append(DDay(title: title, date: date, isFromToday: isFromToday))
+        ddaylist.append(DDay(title: title, date: date, isFromToday: isFromToday))        
     }
 }
